8895354cc7f57f56fe5adc19b856f43f359f41df
kkaique <kaique.s.reis2009@gmail.com>
2026-08-31 09:06:45 -0300

    Atualizei tratamento_excessoes.py
    Adicionei a classe de exceção SaldoInsuficiente, adicionei uma entrada de valores para sacar dinâmica e restringi o saque para que a conta não fique negativa.

--- 31-08/tratamento_excessoes.py
+++ 31-08/tratamento_excessoes.py
@@ -1,3 +1,7 @@
+class SaldoInsuficiente(Exception):
+    def __init__(self) -> None:
+        super().__init__("Saldo Insuficiente!")
+
 class ContaBancaria:
     def __init__(self, titular:str, saldo:float) -> None:
         self.titular = titular
@@ -8,21 +12,39 @@ class ContaBancaria:
 
     def saque(self, valor:float) -> None:
         if valor > 0:
+            if valor > self.saldo:
+                raise SaldoInsuficiente()
             self.__saldo -= valor
 
-valores = [200.00, 350.00, 47.50]
+valores = []
 nome = input("Informe o nome do(a) titular: \n")
 inicial = float(input("Qual o saldo inicial? \n"))
 conta = ContaBancaria(nome, inicial)
 
-for valor in valores:
+es = input("Deseja inserir um valor na sua lista de saques? (s/n) \n")
+while es == 's':
     try:
-        conta.saque(valor)
+        valor = float(input("Insira o valor: "))
+        valores.append(valor)
+        es = input("Deseja inserir um valor na sua lista de saques? (s/n) \n")
     except ValueError:
-        print(f"Valor R${valor} inválido para o saque! ")
-    except:
-        print("Ocorreu um erro no sistema. ")
-    else:
-        print(f"Saque de R${valor} realizado com sucesso! ")
-    finally:
-        print("Conexões finalizadas. ")
+        print("Valor inválido!")
+    if es != 's':
+        break
+
+if len(valores) == 0:
+    print("Nenhum valor para sacar! ")
+else:
+    for valor in valores:
+        try:
+            conta.saque(valor)
+        except ValueError:
+            print(f"Valor R${valor} inválido para o saque! ")
+        except SaldoInsuficiente as t:
+            print(t)
+        except:
+            print("Ocorreu um erro no sistema. ")
+        else:
+            print(f"Saque de R${valor} realizado com sucesso! ")
+        finally:
+            print("Conexões finalizadas. ")
